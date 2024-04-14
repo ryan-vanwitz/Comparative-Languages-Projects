1bc52d8a375db4162ec3a4a9ed0704cae71c1dd5
Ryan Van Witzenburg <vanwitrf@miamioh.edu>
2024-04-14 12:36:35 -0400

    Created Zipcode.cs zipcode initialization class

---
+++ Homework4/Zipcode.cs
@@ -0,0 +1,58 @@
+using System;
+
+// Define the Zipcode class
+public class Zipcode
+{
+    // Properties to represent different attributes of a zipcode
+    public int RecordNumber { get; set; }
+    public string Code { get; set; }
+    public string ZipCodeType { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string LocationType { get; set; }
+    public double Lat { get; set; }
+    public double Long { get; set; }
+    public double Xaxis { get; set; }
+    public double Yaxis { get; set; }
+    public double Zaxis { get; set; }
+    public string WorldRegion { get; set; }
+    public string Country { get; set; }
+    public string LocationText { get; set; }
+    public string Location { get; set; }
+    public bool Decommissioned { get; set; }
+    public int TaxReturnsFiled { get; set; }
+    public int EstimatedPopulation { get; set; }
+    public int TotalWages { get; set; }
+    public string Notes { get; set; }
+
+    // Constructor to initialize zipcode object
+    public Zipcode(int recordNumber, string code, string zipCodeType, string city, string state, string locationType, double lat, double longitude, double xaxis, double yaxis, double zaxis, string worldRegion, string country, string locationText, string location, bool decommissioned, int taxReturnsFiled, int estimatedPopulation, int totalWages, string notes)
+    {
+        RecordNumber = recordNumber;
+        Code = code;
+        ZipCodeType = zipCodeType;
+        City = city;
+        State = state;
+        LocationType = locationType;
+        Lat = lat;
+        Long = longitude;
+        Xaxis = xaxis;
+        Yaxis = yaxis;
+        Zaxis = zaxis;
+        WorldRegion = worldRegion;
+        Country = country;
+        LocationText = locationText;
+        Location = location;
+        Decommissioned = decommissioned;
+        TaxReturnsFiled = taxReturnsFiled;
+        EstimatedPopulation = estimatedPopulation;
+        TotalWages = totalWages;
+        Notes = notes;
+    }
+
+    // Override ToString method to provide a string representation of the zipcode object
+    public override string ToString()
+    {
+        return $"{RecordNumber}\t{Code}\t{ZipCodeType}\t{City}\t{State}\t{LocationType}\t{Lat}\t{Long}\t{Xaxis}\t{Yaxis}\t{Zaxis}\t{WorldRegion}\t{Country}\t{LocationText}\t{Location}\t{Decommissioned}\t{TaxReturnsFiled}\t{EstimatedPopulation}\t{TotalWages}\t{Notes}";
+    }
+}
