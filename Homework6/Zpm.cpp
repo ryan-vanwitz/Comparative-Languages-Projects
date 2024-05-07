991d51bc7e6de2a6be165349cb0706d557d00c94
Ryan Van Witzenburg <vanwitrf@miamioh.edu>
2024-05-07 19:48:41 -0400

    Added command-line argument scanner

--- Homework6/Zpm.cpp
+++ Homework6/Zpm.cpp
@@ -1,6 +1,19 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        cout << "Need to add C++ Zpm file to run statement" << endl;
+        return 1;
+    }
+
+    string fileName = argv[1]; // Get the filename from command-line arguments
+    // Code to run the script specified by the filename
+    cout << "Running script: " << fileName << endl;
+
     return 0;
 }
